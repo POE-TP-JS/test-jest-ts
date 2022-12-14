ac263cb1a2d824e254d1f12c44252b3d301b46d3
Christian Lisangola <jochri3@gmail.com>
2022-12-15 00:36:22 +0300

    Add divide and multiply

--- src/math.ts
+++ src/math.ts
@@ -6,3 +6,11 @@ export function add(a:number,b:number):number{
 export function substract(a:number,b:number):number{
     return a-b;
 }
+
+export function divide(a:number,b:number):number{
+    return a/b;
+}
+
+export function multiply(a:number,b:number):number{
+    return a*b;
+}
